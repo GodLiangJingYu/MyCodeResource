3f6fd69ff8ddf3cfc24102171d71cc312e29289b
梁景毓 <3445078775@qq.com>
2025-06-21 00:03:00 +0800

    11

---
+++ 实习面试/C&C++/Cpp.cpp
@@ -0,0 +1,26 @@
+//
+// Created by 86180 on 25-6-20.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*cout<<"hello world\n"<<endl;
+    cout<<"hello world"<< 123<<endl;
+    int i,j;
+    cin>>i>>j;*/
+   /*typedef*/
+    /*typedef int jj;
+    jj k=0;
+    cout<<k<<endl;
+    const int qb=10;
+    cout<<qb<<endl;*/
+    /*数组*/
+    /*int array[10];
+    for(int i=0;i<10;i++){
+        array[i]=i;
+    }
+    int* parry=&array[0];
+    cout<<parry<<endl;*/
+    return 0;
+}
---
+++ 实习面试/C&C++/数据结构.c
@@ -0,0 +1,6 @@
+//
+// Created by 86180 on 25-6-20.
+//
+#include <stdio.h>
+
+int main()
