57b5b3947ca14d7635a26aad2fe6efb836613863
梁景毓 <3445078775@qq.com>
2025-07-16 18:36:18 +0800

    Add LeetCode 8.cpp and update 6.cpp implementation
    Added a new solution file 8.cpp for LeetCode problem and updated 6.cpp with a vector-based approach for zigzag conversion. Also included encoding configuration for 8.cpp and updated related runtime binaries.

--- algorithm/LeetCode/6.cpp
+++ algorithm/LeetCode/6.cpp
@@ -3,24 +3,36 @@
 //
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 // 这里声明 convert 函数，具体实现你自己补全
 string convert(string s, int numRows){
     string res;
+    int T = 2 * numRows - 2 , vec = 0;
+    vector<string> vectors(numRows);
     if (numRows <= 1 || numRows >= s.size()) {
         return s; // 如果行数小于等于1或大于等于字符串长度，直接返回原字符串
     }
-
+    for (int i = 0 ; i < s.size() ; i++) {
+        if (i % T < numRows - 1) {
+            vectors[vec++].push_back(s[i]);
+        }else {
+            vectors[vec--].push_back(s[i]);
+        }
+    }
+    for (string vector : vectors) {
+        res += vector;
+    }
     return res;
 }
 
 int main() {
     string s;
     int numRows;
-    cout << "s: ";
+    cout << "s:";
     cin >> s;
-    cout << "rows: ";
+    cout << "rows:";
     cin >> numRows;
     cout << convert(s, numRows) << endl;
     return 0;
---
+++ algorithm/LeetCode/8.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 86180 on 25-7-16.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+int myAtoi(string s){
+    int res;
+    
+    return res;
+}
+
+int main() {
+    string s;
+    cout << "ÊäÈë:";
+    getline(cin, s);
+    int result = myAtoi(s);
+    cout << "Êä³ö:" << result << endl;
+    return 0;
+}
